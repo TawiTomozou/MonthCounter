049221a408671e6bf48979e73038c21cef32bd51
Tomozou <hiTomozou@gmail.com>
2017-03-12 13:49:41 +0900

    ソリューションの追加

---
+++ src/MonthCounter/FormMonthCounter.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace MonthCounter
+{
+    partial class formMonthCounter
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMonthCounter));
+            this.buttonTodayCountUp = new System.Windows.Forms.Button();
+            this.buttonTodayCountDown = new System.Windows.Forms.Button();
+            this.textBoxTodayCount = new System.Windows.Forms.TextBox();
+            this.labelToday = new System.Windows.Forms.Label();
+            this.labelYesterday = new System.Windows.Forms.Label();
+            this.textBoxYesterdayCount = new System.Windows.Forms.TextBox();
+            this.buttonYesterdayCountDown = new System.Windows.Forms.Button();
+            this.buttonYesterdayCountUp = new System.Windows.Forms.Button();
+            this.labelMonth = new System.Windows.Forms.Label();
+            this.textBoxMonthCount = new System.Windows.Forms.TextBox();
+            this.buttonMonthCountDown = new System.Windows.Forms.Button();
+            this.buttonMonthCountUp = new System.Windows.Forms.Button();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.timerCheckDate = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonTodayCountUp
+            // 
+            this.buttonTodayCountUp.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonTodayCountUp.Location = new System.Drawing.Point(12, 52);
+            this.buttonTodayCountUp.Name = "buttonTodayCountUp";
+            this.buttonTodayCountUp.Size = new System.Drawing.Size(83, 31);
+            this.buttonTodayCountUp.TabIndex = 2;
+            this.buttonTodayCountUp.Text = "+1";
+            this.buttonTodayCountUp.UseVisualStyleBackColor = true;
+            this.buttonTodayCountUp.Click += new System.EventHandler(this.buttonTodayCountUp_Click);
+            // 
+            // buttonTodayCountDown
+            // 
+            this.buttonTodayCountDown.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonTodayCountDown.Location = new System.Drawing.Point(12, 120);
+            this.buttonTodayCountDown.Name = "buttonTodayCountDown";
+            this.buttonTodayCountDown.Size = new System.Drawing.Size(83, 31);
+            this.buttonTodayCountDown.TabIndex = 4;
+            this.buttonTodayCountDown.Text = "-1";
+            this.buttonTodayCountDown.UseVisualStyleBackColor = true;
+            this.buttonTodayCountDown.Click += new System.EventHandler(this.buttonTodayCountDown_Click);
+            // 
+            // textBoxTodayCount
+            // 
+            this.textBoxTodayCount.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxTodayCount.Location = new System.Drawing.Point(28, 89);
+            this.textBoxTodayCount.Name = "textBoxTodayCount";
+            this.textBoxTodayCount.ReadOnly = true;
+            this.textBoxTodayCount.Size = new System.Drawing.Size(50, 25);
+            this.textBoxTodayCount.TabIndex = 3;
+            this.textBoxTodayCount.Text = "0000";
+            this.textBoxTodayCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelToday
+            // 
+            this.labelToday.AutoSize = true;
+            this.labelToday.Location = new System.Drawing.Point(35, 34);
+            this.labelToday.Name = "labelToday";
+            this.labelToday.Size = new System.Drawing.Size(37, 15);
+            this.labelToday.TabIndex = 1;
+            this.labelToday.Text = "今日";
+            // 
+            // labelYesterday
+            // 
+            this.labelYesterday.AutoSize = true;
+            this.labelYesterday.Location = new System.Drawing.Point(138, 34);
+            this.labelYesterday.Name = "labelYesterday";
+            this.labelYesterday.Size = new System.Drawing.Size(37, 15);
+            this.labelYesterday.TabIndex = 5;
+            this.labelYesterday.Text = "昨日";
+            // 
+            // textBoxYesterdayCount
+            // 
+            this.textBoxYesterdayCount.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxYesterdayCount.Location = new System.Drawing.Point(131, 89);
+            this.textBoxYesterdayCount.Name = "textBoxYesterdayCount";
+            this.textBoxYesterdayCount.ReadOnly = true;
+            this.textBoxYesterdayCount.Size = new System.Drawing.Size(50, 25);
+            this.textBoxYesterdayCount.TabIndex = 7;
+            this.textBoxYesterdayCount.Text = "0000";
+            this.textBoxYesterdayCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonYesterdayCountDown
+            // 
+            this.buttonYesterdayCountDown.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonYesterdayCountDown.Location = new System.Drawing.Point(115, 120);
+            this.buttonYesterdayCountDown.Name = "buttonYesterdayCountDown";
+            this.buttonYesterdayCountDown.Size = new System.Drawing.Size(83, 31);
+            this.buttonYesterdayCountDown.TabIndex = 8;
+            this.buttonYesterdayCountDown.Text = "-1";
+            this.buttonYesterdayCountDown.UseVisualStyleBackColor = true;
+            this.buttonYesterdayCountDown.Click += new System.EventHandler(this.buttonYesterdayCountDown_Click);
+            // 
+            // buttonYesterdayCountUp
+            // 
+            this.buttonYesterdayCountUp.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonYesterdayCountUp.Location = new System.Drawing.Point(115, 52);
+            this.buttonYesterdayCountUp.Name = "buttonYesterdayCountUp";
+            this.buttonYesterdayCountUp.Size = new System.Drawing.Size(83, 31);
+            this.buttonYesterdayCountUp.TabIndex = 6;
+            this.buttonYesterdayCountUp.Text = "+1";
+            this.buttonYesterdayCountUp.UseVisualStyleBackColor = true;
+            this.buttonYesterdayCountUp.Click += new System.EventHandler(this.buttonYesterdayCountUp_Click);
+            // 
+            // labelMonth
+            // 
+            this.labelMonth.AutoSize = true;
+            this.labelMonth.Location = new System.Drawing.Point(225, 34);
+            this.labelMonth.Name = "labelMonth";
+            this.labelMonth.Size = new System.Drawing.Size(67, 15);
+            this.labelMonth.TabIndex = 9;
+            this.labelMonth.Text = "今月累計";
+            // 
+            // textBoxMonthCount
+            // 
+            this.textBoxMonthCount.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxMonthCount.Location = new System.Drawing.Point(233, 89);
+            this.textBoxMonthCount.Name = "textBoxMonthCount";
+            this.textBoxMonthCount.ReadOnly = true;
+            this.textBoxMonthCount.Size = new System.Drawing.Size(50, 25);
+            this.textBoxMonthCount.TabIndex = 11;
+            this.textBoxMonthCount.Text = "0000";
+            this.textBoxMonthCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonMonthCountDown
+            // 
+            this.buttonMonthCountDown.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonMonthCountDown.Location = new System.Drawing.Point(217, 120);
+            this.buttonMonthCountDown.Name = "buttonMonthCountDown";
+            this.buttonMonthCountDown.Size = new System.Drawing.Size(83, 31);
+            this.buttonMonthCountDown.TabIndex = 12;
+            this.buttonMonthCountDown.Text = "-1";
+            this.buttonMonthCountDown.UseVisualStyleBackColor = true;
+            this.buttonMonthCountDown.Click += new System.EventHandler(this.buttonMonthCountDown_Click);
+            // 
+            // buttonMonthCountUp
+            // 
+            this.buttonMonthCountUp.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonMonthCountUp.Location = new System.Drawing.Point(217, 52);
+            this.buttonMonthCountUp.Name = "buttonMonthCountUp";
+            this.buttonMonthCountUp.Size = new System.Drawing.Size(83, 31);
+            this.buttonMonthCountUp.TabIndex = 10;
+            this.buttonMonthCountUp.Text = "+1";
+            this.buttonMonthCountUp.UseVisualStyleBackColor = true;
+            this.buttonMonthCountUp.Click += new System.EventHandler(this.buttonMonthCountUp_Click);
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelDate.Location = new System.Drawing.Point(12, 9);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(119, 20);
+            this.labelDate.TabIndex = 0;
+            this.labelDate.Text = "2000/01/01";
+            // 
+            // timerCheckDate
+            // 
+            this.timerCheckDate.Interval = 1000;
+            this.timerCheckDate.Tick += new System.EventHandler(this.timerCheckDate_Tick);
+            // 
+            // formMonthCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 173);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelMonth);
+            this.Controls.Add(this.textBoxMonthCount);
+            this.Controls.Add(this.buttonMonthCountDown);
+            this.Controls.Add(this.buttonMonthCountUp);
+            this.Controls.Add(this.labelYesterday);
+            this.Controls.Add(this.textBoxYesterdayCount);
+            this.Controls.Add(this.buttonYesterdayCountDown);
+            this.Controls.Add(this.buttonYesterdayCountUp);
+            this.Controls.Add(this.labelToday);
+            this.Controls.Add(this.textBoxTodayCount);
+            this.Controls.Add(this.buttonTodayCountDown);
+            this.Controls.Add(this.buttonTodayCountUp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(100, 100);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formMonthCounter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "月累計カウンター";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.formMonthCounter_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTodayCountUp;
+        private System.Windows.Forms.Button buttonTodayCountDown;
+        private System.Windows.Forms.TextBox textBoxTodayCount;
+        private System.Windows.Forms.Label labelToday;
+        private System.Windows.Forms.Label labelYesterday;
+        private System.Windows.Forms.TextBox textBoxYesterdayCount;
+        private System.Windows.Forms.Button buttonYesterdayCountDown;
+        private System.Windows.Forms.Button buttonYesterdayCountUp;
+        private System.Windows.Forms.Label labelMonth;
+        private System.Windows.Forms.TextBox textBoxMonthCount;
+        private System.Windows.Forms.Button buttonMonthCountDown;
+        private System.Windows.Forms.Button buttonMonthCountUp;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Timer timerCheckDate;
+    }
+}
+
---
+++ src/MonthCounter/FormMonthCounter.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MonthCounter
+{
+    public partial class formMonthCounter : Form
+    {
+        //定数
+        const ushort INITIALCOUNTER =0;         //カウンター初期値
+        const ushort COUNTVALUE = 1;            //カウント増減値
+        const ushort LIMITMAX = 9999;           //カウンター最大値
+        const ushort LIMITMIN = 0;              //カウンター最小値
+
+        //変数
+        private DateTime dateToday;             //本日の日付
+        private ushort ushortTodayCount;        //今日のカウント数
+        private ushort ushortYesterdayCount;    //昨日のカウント数
+        private ushort ushortMonthCount;        //月累計のカウント数
+        public formMonthCounter()
+        {
+            InitializeComponent();
+            this.Location = new Point(Properties.Settings.Default.X, Properties.Settings.Default.Y);
+            //カウント数日付初期化
+            initialCountDate();
+            //Timer起動
+            timerCheckDate.Start();
+        }
+
+        //月日付変更チェック
+        private void checkChangeMonthDate()
+        {
+            DateTime dateNow = DateTime.Now.Date;
+            //日付変更チェック
+            if (dateToday != dateNow)
+            {
+                //年か月変更チェック
+                if (dateToday.Year != dateNow.Year || dateToday.Month != dateNow.Month)
+                {
+                    //月をまたいだら月累計と昨日のカウント数初期化
+                    ushortMonthCount = INITIALCOUNTER;
+                    ushortYesterdayCount = INITIALCOUNTER;
+                    textBoxMonthCount.Text = ushortMonthCount.ToString("0000");
+                }
+                //月をまたいでない場合、1日差なら今日のカウント数を昨日へコピー
+                else if ((dateNow - dateToday).Days == 1) ushortYesterdayCount = ushortTodayCount;
+                else ushortYesterdayCount = INITIALCOUNTER;
+                //今日のカウント数を初期化
+                ushortTodayCount = INITIALCOUNTER;
+                labelDate.Text = dateNow.ToString("yyyy/MM/dd");
+                textBoxYesterdayCount.Text = ushortYesterdayCount.ToString("0000");
+                textBoxTodayCount.Text = ushortTodayCount.ToString("0000"); ;
+                //日付の更新は最後に行う
+                dateToday = dateNow;
+            }
+        }
+        //カウント日付初期化
+        private void initialCountDate()
+        {
+            ushortTodayCount = Properties.Settings.Default.TodayCount;
+            ushortYesterdayCount = Properties.Settings.Default.YesterdayCount;
+            ushortMonthCount = Properties.Settings.Default.MonthCount;
+            textBoxTodayCount.Text = ushortTodayCount.ToString("0000");
+            textBoxYesterdayCount.Text = ushortYesterdayCount.ToString("0000");
+            textBoxMonthCount.Text = ushortMonthCount.ToString("0000");
+            //今日の日付挿入 前回起動時と日付が変わっていたら日付変更チェックを入れる
+            dateToday = Properties.Settings.Default.dateToday;
+            checkChangeMonthDate();
+            labelDate.Text = dateToday.ToString("yyyy/MM/dd");
+
+        }
+
+        //カウントの数値増減
+        //CountUpDownがTrueならUp、FalseならDown
+        private bool changeCount(TextBox textBoxCount,ref ushort ushortCount, bool CountUpDown)
+        {
+            if (CountUpDown)
+            {
+                //LIMITMAXを超えないようにする
+                if (ushortCount == LIMITMAX)
+                {
+                    return false;
+                }
+                ushortCount += COUNTVALUE;
+            }
+            else
+            {
+                //LIMITMINを下回らないようにする
+                if (ushortCount == LIMITMIN)
+                {
+                    return false;
+                }
+                ushortCount -= COUNTVALUE;
+            }
+            textBoxCount.Text = ushortCount.ToString("0000");
+            return true;
+        }
+
+        private void timerCheckDate_Tick(object sender, EventArgs e)
+        {
+            //処理中タイマーを止める
+            timerCheckDate.Stop();
+            checkChangeMonthDate();
+            //タイマーを再開する
+            timerCheckDate.Start();
+        }
+
+        private void buttonTodayCountUp_Click(object sender, EventArgs e)
+        {
+            if (changeCount(textBoxTodayCount, ref ushortTodayCount, true))
+            {
+                changeCount(textBoxMonthCount, ref ushortMonthCount, true);
+            }
+        }
+
+        private void buttonTodayCountDown_Click(object sender, EventArgs e)
+        {
+            
+            
+            if(changeCount(textBoxTodayCount, ref ushortTodayCount, false))
+            {
+                changeCount(textBoxMonthCount, ref ushortMonthCount, false);
+            }
+        }
+
+        private void buttonYesterdayCountUp_Click(object sender, EventArgs e)
+        {
+            if (changeCount(textBoxYesterdayCount, ref ushortYesterdayCount, true))
+            {
+                changeCount(textBoxMonthCount, ref ushortMonthCount, true);
+            }
+        }
+
+        private void buttonYesterdayCountDown_Click(object sender, EventArgs e)
+        {
+            if (changeCount(textBoxYesterdayCount, ref ushortYesterdayCount, false))
+            {
+                changeCount(textBoxMonthCount, ref ushortMonthCount, false);
+            }
+        }
+
+        private void buttonMonthCountUp_Click(object sender, EventArgs e)
+        {
+            changeCount(textBoxMonthCount, ref ushortMonthCount, true);
+        }
+
+        private void buttonMonthCountDown_Click(object sender, EventArgs e)
+        {
+            changeCount(textBoxMonthCount, ref ushortMonthCount, false);
+        }
+
+        private void formMonthCounter_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.X = this.Location.X;
+            Properties.Settings.Default.Y = this.Location.Y;
+            Properties.Settings.Default.TodayCount = ushortTodayCount;
+            Properties.Settings.Default.YesterdayCount = ushortYesterdayCount;
+            Properties.Settings.Default.MonthCount = ushortMonthCount;
+            Properties.Settings.Default.dateToday = dateToday;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
